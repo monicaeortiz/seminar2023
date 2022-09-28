e31f28742fabd0b38db9dbb7e09ab751fc992c9d
Tara <tsalli@greenwichacademy.org>
2022-09-28 11:44:09 -0400

    Added the remove method

--- unit0/LizzieList.java
+++ unit0/LizzieList.java
@@ -70,6 +70,26 @@ public class ourList {
         return "invalid index";
     }
 
+    public String remove(int index) {
+        // check if the given index is a valid index in the array (not larger than the length or negative)
+        if (index>logicalSize||index<0) {
+            // if not, return "-1"
+            return "-1";
+        }
+        else {
+            // saves the item that is to be removed in a String variable
+            String toRemove = mainArray[index];
+            // iterates through the array beginning at the index of the item to be removed and replaces each item with the one that follows it
+            for (int i = index; i<logicalSize-1; i++) {
+               mainArray[i] = mainArray[i+1];
+            }
+            // decrements the logical size of the array by one to signal that an item has been removed
+            logicalSize--;
+            // returns the item that was removed
+            return toeRemove;
+        }
+    }
+
     public static void main(String[] args){
         String[] arr = {"a", "b", "c", "d", "e"};
         arr.add("g");
