842311c5b366349266bd119c6f4e1531cb58cbf8
toylmpia <tozhu3@gmail.com>
2022-12-01 09:15:22 -0500

    contains3 method and demonstration of how to throw an error

--- unit2/SetsAndMapsInClass.java
+++ unit2/SetsAndMapsInClass.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class SetsAndMapsInClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        exceptionThrowingExample(false);  // Change parameter to true to get the error message.
+
         HashSet<String> fruits = new HashSet<>();
         fruits.add("banana");
         fruits.add("orange");
@@ -44,6 +46,33 @@ public class SetsAndMapsInClass {
 
         // Check if bowl1 is a superset of bowl2.
         // System.out.println(bowl1.containsAll(bowl2));
+
+        // Initialize a new map.
+        Map<String, Double> salaries = new HashMap<>();
+        // Keys are strings, and values are doubles.
+        salaries.put("Ross", 100.0);
+        salaries.put("Janice", 103.6);
+        salaries.put("Chandler", 12000.0);
+
+        salaries.put("Chandler", -1.0);
+
+        // Iterate through the keys of the map.
+        Set<String> names = salaries.keySet();
+        System.out.println("Names in the map");
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        Collection<Double> money = salaries.values();
+        // Iterate through the values of the map.
+        for (Double salary : money) {
+            System.out.println(salary);
+        }
+
+        birthdays();
+
+        List<String> words = new ArrayList<String>(Arrays.asList("a", "a", "a", "b"));
+        System.out.println("The list contains a word that appears at least 3 times: " + contains3(words));
     }
 
     public static void sortAndRemoveDuplicates(List<Integer> nums) {
@@ -56,4 +85,61 @@ public class SetsAndMapsInClass {
         nums.clear();
         nums.addAll(sortedList);
     }
+
+    public static void birthdays() {
+        Map<String, String> birthdays = new HashMap<>();
+        birthdays.put("TS", "Dec");
+        birthdays.put("Eliza", "Nov");
+        birthdays.put("Tara", "May");
+
+        Set<String> names = birthdays.keySet();
+        for (String name : names) {
+            System.out.println(name + " was born on " + birthdays.get(name));
+        }
+
+    }
+
+    public static boolean contains3(List<String> words) {
+        // Create a frequency map - map of the counts of each word.
+        Map<String, Integer> counts = new HashMap<>();
+        // Populate it by iterating through words and adding each unique word
+        // as the key.
+        // The values will be the corresponding counts for each word.
+        for (String word : words) {
+            // Add the word to counts and update its corresponding value.
+            counts.put(word, counts.getOrDefault(word, 0) + 1); 
+
+            // ALTERNATIVELY:
+            // if (counts.containsKey(word)) {
+            //     // Get the previous count of this word in counts.
+            //     int count = counts.get(word) + 1;
+            //     // Update the map of counts.
+            //     counts.put(word, count);
+            // } else { // word is NOT in counts yet.
+            //     counts.put(word, 1);
+            // }
+        }
+        System.out.println(counts);
+        // Is there a value in the dictionary that is >= 3.
+        // Get all the values at once.
+        Collection<Integer> countNums = counts.values();
+        // Iterate through countNums.
+        for (Integer count : countNums) {
+            // If one of them is >= 3, return true.
+            if (count >= 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Method to demonstrate how to throw an exception!
+    public static void exceptionThrowingExample(boolean problem) throws Exception {
+        if (problem) { // Some condition...
+            throw new Exception("A specific error message goes here - what went wrong?");
+        } else {
+            System.out.println("Everything is fine");
+        }
+    }
+
 }
