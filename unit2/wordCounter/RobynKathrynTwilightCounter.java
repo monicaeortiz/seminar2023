a58ee2912be0a5fde436f1ad0ab97be8ef784ea5
Kathryn Xia <kxia@greenwichacademy.org>
2022-12-06 21:00:06 -0500

    removed unused code

--- unit2/wordCounter/RobynKathrynTwilightCounter.java
+++ unit2/wordCounter/RobynKathrynTwilightCounter.java
@@ -6,17 +6,12 @@ public class RobynKathrynTwilightCounter {
     public static void main (String[] commandLineArgs) throws FileNotFoundException{  
         String string = commandLineArgs[0]; 
         File filename = new File(string); 
-        findPopWords(filename, 10, "popWords.txt"); 
+        findPopWords(filename, 25, "popWords.txt"); 
     }
 
     public static void findPopWords(File f, int numWords, String fileName) throws FileNotFoundException{
-        // ArrayList<String> uniqueWords = new ArrayList<String>(); //will hold 
-        // ArrayList<Integer> wordsCount = new ArrayList<Integer>();
         Map<String, Integer> wordFreq = new HashMap<>(); 
-
         Set<String> highestCountWords = new HashSet<>();
-
-        int highestCount = 0;
 
 
         Set<String> stopWords = new HashSet<>(); //creates set to hold stop words
@@ -30,61 +25,44 @@ public class RobynKathrynTwilightCounter {
         }
         stopWScanner.close(); 
 
+
+
         Scanner fileScan = new Scanner(f);
 
         while (fileScan.hasNextLine()) {
-            String line = fileScan.nextLine();
-            Scanner lineScan = new Scanner(line);
-            while (lineScan.hasNext()) {
-                String curWord = lineScan.next(); //gets each word as a token
-                curWord = cleanUp(curWord);
-                if (!(stopWords.contains(curWord)) && curWord.length() >0) { //if the current word is not a stop word and not a space, do not add in to the map 
-                    wordFreq.put(curWord, wordFreq.getOrDefault(curWord, 0) + 1);
+            // String line = fileScan.nextLine();
+            // Scanner lineScan = new Scanner(line);
+            String[] row = fileScan.nextLine().split(" "); //takes a line, gets rid of spaces, and puts elements in an array
+            for (int k = 0; k < row.length; k++) {
+                String curWord = row[k]; 
+                curWord = cleanUp(curWord);//removing punctuation
+                if (!stopWords.contains(curWord)) { //if the current word is a stop word, do not add in to the map 
+                    wordFreq.put(curWord, wordFreq.getOrDefault(curWord, 0) + 1);//either increase the value of the key by 1, or add the word into the map and set the default value to 1
                 }
             }
-            lineScan.close(); 
         }
         fileScan.close(); 
 
         List<String> uniqueWords = new ArrayList<>(wordFreq.keySet());
 
-        PrintStream p = new PrintStream (fileName);
+        PrintStream p = new PrintStream (fileName);//prinstream to print our most frequent words to a file
 
         for (int l = 0; l < numWords; l++){
-        String mostFreqWord = uniqueWords.get(0); 
-        int largestValue = wordFreq.get(mostFreqWord); 
+        String mostFreqWord = uniqueWords.get(0); //initializing most frequent word to the first element in our list
+        int largestValue = wordFreq.get(mostFreqWord); //largest value intitialized to the value of mostFreqWord
 
             for (int i = uniqueWords.size() - 1; i >= 0; i--) { //checking values 
-                if (wordFreq.get(uniqueWords.get(i)) > largestValue) { 
+                if (wordFreq.get(uniqueWords.get(i)) > largestValue) { //if we find a more frequent word, replace mostFreqWord and largestValue accordingly
                 largestValue = wordFreq.get(uniqueWords.get(i)); 
                 mostFreqWord = uniqueWords.get(i); 
                 
                 }
             }
 
-            p.println(mostFreqWord);
-            uniqueWords.remove(mostFreqWord);
+            p.println(l + " place: " + mostFreqWord);
+            uniqueWords.remove(mostFreqWord);//remove the mostFreqWord from the list, no longer included in comparisons
 
         }
-
-        // Map<String, Integer> highestWordCounts = new TreeMap<>();
-        // int largestCount = 0;
-        // for ()
-
-
-        // Set<String> mostUsedWords = new HashSet<String>();
-        // Set<Integer> mostUsedWordsCounts = new HashSet<Integer>();
-
-        // for (int i = 0; i < numWords; i++) {
-
-        //     mostUsedWords.add(uniqueWords.get(findLargest(wordsCount)));
-        //     mostUsedWordsCounts.add(wordsCount.get(findLargest(wordsCount))); 
-        //     uniqueWords.remove(uniqueWords.get(findLargest(wordsCount)));
-        //     wordsCount.remove(wordsCount.get(findLargest(wordsCount)));
-        // }
-        // System.out.println("Top " + numWords + " most used words: " + mostUsedWords);
-        // System.out.println("  Their counts: " + mostUsedWordsCounts);
-
 
     }
     
@@ -96,39 +74,7 @@ public class RobynKathrynTwilightCounter {
         return word;
     }
 
-    public static String findSmallest(Map <String, Integer> map) {
-        Collection<String> words = map.keySet();
-        int highestCount = 0;
-        String highestWord = "";
-        int count = 0;
-
-            for (String word: words){
-                if (count == 0){
-                    highestCount = map.get(word);
-                    count ++;
-                }
-                if (map.get(word) < highestCount){
-                    highestCount = map.get(word);
-                    highestWord = word;
-                }
-            }
-        return highestWord;
-    }
-
 }
-//     public static int findLargest(ArrayList<Integer> arr) { //returns index of the smallest number
-//     int index = 0; //
-//     int largest = arr.get(0); //starting with the largest possible number
-
-//     for (int i = 0; i < arr.size(); i++) {
-//       if (arr.get(i) > largest) { //checking it element does not equal -1
-//         largest = arr.get(i); //updates each time a smaller number is found
-//         index = i; //records index of smallest number
-//       }
-
-//   }
-
-   // return index;
 
 
 // Read file 
