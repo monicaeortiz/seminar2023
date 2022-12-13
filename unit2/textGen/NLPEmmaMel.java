c313671e15dd57b19b90ad9a82153e73f722f1e6
etrauber <emma.trauber@gmail.com>
2022-12-13 10:13:14 -0500

    adding starter code for main printNewScript method

--- unit2/textGen/NLPEmmaMel.java
+++ unit2/textGen/NLPEmmaMel.java
@@ -2,16 +2,46 @@ public class NLPEmmaMel{
     public static void main(String[] args){
     }
 
-    public static void printNewScript(String pathname, int numLines){
+    public static void printNewScript(File f, int numLines){
         //read in file using a scanner
-        //create new file with printstream
+        Scanner sc = new Scanner(dataset);
+        //create map to keep bigrams
+        Map<String, ArrayList<String>> bigrams = new HashMap<>();
+
         //loop to keep going until there are no more lines in given file
+        while(sc.hasNextLine()){
             //use .split to separate each line into an arraylist of words - get rid of spaces
                 //keep punctuation on the word
+            String[] line = sc.nextLine().split(",");
+            ArrayList<String> lineList = new ArrayList<>(Arrays.asList(line));
             //loop through the length of the line and add words and followers to map
+            for(int i=0; i<lineList.size(); i++){
+                if(i == lineList.size()-1){
+                    //how do you add one to arraylist of values in same line?
+                    bigrams.put(lineList.get(i), .add(""));
+                }
+                    //how do you add one to arraylist of values in same line?
+                bigrams.put(lineList.get(i), .add(lineList.get(i+1)));
+            }     
+        }
+        sc.close();
+        //create new file with printstream
+        PrintStream p = new PrintStream("Generate file");
         //for loop to randomly pick bigrams from map and runs the numLines, which is amount of lines that are desired in new file
-            //use pickBigram method and print it to new file
+        int counter =0;
+        while(counter < numLines){
+            String bigram = pickBigram(bigrams);
+            //print bigram to new file
+            p.print(bigram);
+            if(bigram.indexOf(" ") == -1){
+                counter++;
+                p.println();
+            }
+        }
+        p.close();
     }
+
+
     // returns a random bigram so that it can be added to the printstream
     public static String pickBigram(Map<String, ArrayList<String>> bigrams){
         // make a variable to hold the key set of the bigram map
@@ -21,6 +51,8 @@ public class NLPEmmaMel{
         // use getRandomElement to get a random value from listOfValues (can call this chosenValue)
         // return a string formatted as follows: chosenKey + " " + chosenValue (this is a bigram)
     }
+
+    
     // picks a random element from an array list
     public static String getRandomElement(ArrayList<String> list){
         // use Math.Random to pick a random index (utilize size of list to make sure all elements of list could be chosen)
