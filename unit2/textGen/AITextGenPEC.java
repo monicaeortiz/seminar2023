dc8505f794608d91861b56e5e12fd79ed22bc8ba
espaulds <espaulding@greenwichacademy.org>
2022-12-13 12:06:00 -0500

    finished parseFile method

--- unit2/textGen/AITextGenPEC.java
+++ unit2/textGen/AITextGenPEC.java
@@ -8,14 +8,44 @@ public class AITextGenPec {
 
     public static void generateNewFile(Map<String, List<String>>){
         pass;
+
     }
 
     public static Map<String, List<String>> parseFile(File f){
-        Map<String, String> bigrams = new HashMap<>();
+        //creating the map for our file 
+        Map<String, List<String>> bigrams = new HashMap<>();
+        String word = "";
+        //create new scanner to read in file
+        Scanner sc = new Scanner(f);
+        //creating a list that will hold the current value (list) for each o the keys 
+        ArrayList<String> currList = new ArrayList<>();
+        while(sc.hasNext()){
+            //creating the current word we're on to store as a key
+            String currWord= sc.next();
+            //gettnig the list that already exists for our key and setting it to currList
+            if(bigrams.contains(word) == false){
+                bigrams.put(word, currWord);
+            } else {
+                currList = bigrams.get(word);
+                //adding the second word to the list of words (vlue) for the key 
+                currList.add(currWord);
+                //putting the current word as a key and the updated list as the value
+                bigrams.put(word, currList);
+            }
+            //reassigning word to the next word
+            word = currWord;
+            
+        }
+        sc.close(); //closing our scanner
+        
+        return bigrams; //returning the results of our map 
     }
+
+    
     
     public static String chooseBigramWord(Map<String, List<String>> bigrams){
-        pass;
+        Set<String> keys = bigrams.keySet();
+        ArrayList<String> keyList = new ArrayList<>(keys);
     }
 
     public static String chooseValue(Map<String, List<String>> bigrams, String word){
