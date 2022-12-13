4b89796279f223c7ea76723c48307fd63350ba66
robynoconnor <robynoconnor1@gmail.com>
2022-12-13 00:36:04 -0500

    Added method headers and some pseudocode for methods

---
+++ unit2/textGen/RobynHannahTextGen.java
@@ -0,0 +1,22 @@
+import java.util.*; 
+import java.io.*;
+
+public class RobynHannahTextGen {
+    public static void main (String [] args) {
+
+    }
+
+    public static string sentenceBuilder (Map<String> bigrams) {
+    //will print output text to prinstream
+    }
+
+    public static Map<String> populateMap (File f) {
+    //will take input text and create bigrams map 
+    //map returned by method to be used in sentenceBuilder and getRandomElement
+    }
+
+    public static string getRandomElement (List<String> strings) {
+    //helper method to be used in sentenceBuilder
+    //will choose a random value from a list of values (that corresponds to a key in bigrams) 
+    }
+}
