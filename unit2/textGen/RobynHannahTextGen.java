36e5cb7ababbda2bbe3dfe500c8797c8cc3a3627
robynoconnor <robynoconnor1@gmail.com>
2022-12-13 09:32:10 -0500

    Fixed syntax errors in method headers

--- unit2/textGen/RobynHannahTextGen.java
+++ unit2/textGen/RobynHannahTextGen.java
@@ -6,16 +6,16 @@ public class RobynHannahTextGen {
 
     }
 
-    public static string sentenceBuilder (Map<String> bigrams) {
+    public static String sentenceBuilder (Map<String, List> bigrams) {
     //will print output text to prinstream
     }
 
-    public static Map<String> populateMap (File f) {
+    public static Map<String, List> populateMap (File f) {
     //will take input text and create bigrams map 
     //map returned by method to be used in sentenceBuilder and getRandomElement
     }
 
-    public static string getRandomElement (List<String> strings) {
+    public static String getRandomElement (List<String> strings) {
     //helper method to be used in sentenceBuilder
     //will choose a random value from a list of values (that corresponds to a key in bigrams) 
     }
