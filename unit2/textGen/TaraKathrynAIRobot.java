939a4472d35613fef299c482f72971b586be5264
Tara <tsalli@greenwichacademy.org>
2022-12-12 22:00:30 -0500

    added tentative skeleton methods

---
+++ unit2/textGen/TaraKathrynAIRobot.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class TaraKathrynAIRobot {
+
+    public static void main (String[] args) {
+
+    }
+
+
+    public static Map scanFile (File textFile) {
+
+    }
+
+    public static void writeNewLine() {
+
+    }
+
+
+    public static String getRandomELement(String currentWord, Map <String, ArrayList <String>> bigrams) {
+
+    }
+
+
+    public static String pickBigram(Map <String, ArrayList <String>> bigrams) {
+
+    }
+
+
+    public static String checkPunctuation(Map <String, ArrayList <String>> bigrams) {
+
+    }
+
+
+
+
+
+
+}
