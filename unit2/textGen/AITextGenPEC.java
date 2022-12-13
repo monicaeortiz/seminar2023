a7331f329f1bd26ecb5c37583cb5acf4d962aee8
pvonoiste1 <patricia@vonoiste.com>
2022-12-13 16:08:46 -0500

    tested chooseBigramWord and chooseValue, both passed test cases

--- unit2/textGen/AITextGenPEC.java
+++ unit2/textGen/AITextGenPEC.java
@@ -1,12 +1,21 @@
 import java.util.*;
-import java.util.io;
 
 public class AITextGenPEC {
     public static void main(String[] args){
+        //testing helper methods
+        Map<String, List<String>> testMap = new HashMap<>();
+        List<String> testValues1 = new ArrayList<>(Arrays.asList("bye", "my", "name"));
+        List<String> testValues2 = new ArrayList<>(Arrays.asList("road", "street", "lane"));
+        List<String> testValues3 = new ArrayList<>(Arrays.asList("love", "you", "thanks"));
+        testMap.put("hello", testValues1);
+        testMap.put("hey", testValues2);
+        testMap.put("my", testValues3);
+        String key = chooseBigramWord(testMap);
+        System.out.println(key);
+        System.out.println(chooseValue(testMap, key));
+
 
     }
-
-    //methods
 
     public static void generateNewFile(Map<String, List<String>> bigrams, int numSentences){
         //make an ArrayList that contains the punctuation
@@ -35,27 +44,19 @@ public class AITextGenPEC {
     
     public static String chooseBigramWord(Map<String, List<String>> bigrams){
         //make key set into arrayList, using the keys as the input into the arrayList
-        Collection<String> keys = new ArrayList<>(bigrams.keySet());
+        ArrayList<String> keys = new ArrayList<>(bigrams.keySet());
         //randomly find a key using math.random
         int range = keys.size();
-        return keys.get((int)(Math.random()*range+0));
+        //return a random one of the keys
+        return keys.get((int)(Math.random()*range));
     }
 
     public static String chooseValue(Map<String, List<String>> bigrams, String word){  
         //get the values
-        Collection<String> values = bigrams.get(word);
+        List<String> values = bigrams.get(word);
         //find the length of the values
-
         //ramdomly find a value
         int range = values.size();
-        return values.get((int)(Math.random()*range+0));
+        return values.get((int)(Math.random()*range));
     }
-
-    //extra helper methods
-    public static String findPunctuation(Map<String, String> bigrams){
-        pass;
-    }
-
-    
-    
 }
