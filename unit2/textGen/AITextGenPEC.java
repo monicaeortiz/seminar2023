35b5c9c25a7601daa865b92f0ada15cfa51febf8
espaulds <espaulding@greenwichacademy.org>
2022-12-12 19:59:00 -0500

    new file with method shells

---
+++ unit2/textGen/AITextGenPEC.java
@@ -0,0 +1,28 @@
+public class AITextGenPec {
+
+    public static void main(String[] args){
+
+    }
+
+    //methods
+
+    public static map parseLine(File f){
+        pass;
+    }
+    
+    public static String chooseBigramWord(Map bigrams){
+        pass;
+    }
+
+    public static String chooseValue(Map bigrams, String word){
+        pass;
+    }
+
+    //extra helper methods
+    public static String findPunctuation(Map bigrams){
+        pass;
+    }
+
+    
+    
+}
