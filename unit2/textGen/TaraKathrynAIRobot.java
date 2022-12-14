fe0ad959aebcad0f593a4ed562f91c7c6284d544
Kathryn Xia <kxia@greenwichacademy.org>
2022-12-14 12:44:47 -0500

    made a method that writes to the file, cannot test it until the scanFile is verifief to be correct, but was able to speudocode

--- unit2/textGen/TaraKathrynAIRobot.java
+++ unit2/textGen/TaraKathrynAIRobot.java
@@ -24,19 +24,69 @@ public class TaraKathrynAIRobot {
     }
 
 
+
+/*
+
+if the word ends with punctiuation, have the value be an empty string
+the sentence starter words will be the values of the empty string key
+
+*/
     // public static Map scanFile (File textFile) {
 
     // }
 
-    // public static void writeNewLine() {
+    /*psuedocode
 
-    // }
+    1. pass in how many words we want the new file to be
+    2. Start with an empty string and get bigrams of the empty string, as those words are likely to be sentence starters. 
+    3. keep on getting the bigram of the word before, until we reach a word with punctuation at the end.
+        increase the number of sentences by q
+        and make the curWord an empty string, so we get a sentence starter word
+
+    */
 
 
-    // public static String getRandomELement(String currentWord, Map <String, ArrayList <String>> bigrams) {
 
-    // }
+    public static void writeNewFile(Map <String, ArrayList<String>> bigrams int numSentences, String fileName) {
 
+        ArrayList<String> sentenceEnder = new ArrayList<>(Arrays.asList(".", "?", "!"));
+
+        PrintStream p = new PrintStream(fileName);
+
+        String newText = "";
+        String curWord = "";
+        String newWord = "";
+
+        int curNumSentences = 0;
+
+        while (curNumSentences <= numSentences){
+
+            newWord = pickBigram(bigrams, curWord);
+            newText += newWord;
+            curWord = newWord;
+
+            for (int i = 0; i < 3; i ++){
+                 if (curWord.indexOf(sentenceEnder.get(i)) != -1){
+                    curWord = "";
+                    curNumSentences ++;
+                 }
+
+            }
+        }
+
+        p.print(newText);
+
+    }
+
+
+
+/*pseudocode, lerning my lesson slowly but surely
+
+1. use .get to get the arraylist(value) of a specific String (key)
+2. Use Math.random to randomely generate a number (withing the indeices of the arrayList). 
+Elements that appear more often in the arraylist will be more likely to show up
+
+*/
 
     public static String pickBigram(Map <String, ArrayList <String>> bigrams, String key) {
 
@@ -47,11 +97,24 @@ public class TaraKathrynAIRobot {
 
     // public static String checkPunctuation(Map <String, ArrayList <String>> bigrams) {
 
+
+
     // }
 
 
+/* do while loop
+
+get the first word in the file
+add it to the map
 
 
 
+next loop
+add a value to the previous string, which is the current word
+
+
+
+
+*/
 
 }
