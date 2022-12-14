812f06c425a0c61ed5a5c10517a71dbe152b728b
Kathryn Xia <kxia@greenwichacademy.org>
2022-12-14 14:20:39 -0500

    It works

--- unit2/textGen/TaraKathrynAIRobot.java
+++ unit2/textGen/TaraKathrynAIRobot.java
@@ -4,49 +4,69 @@ public class TaraKathrynAIRobot {
 
     public static void main (String[] args) throws FileNotFoundException {
 
-        File test = new File("macbeth.txt");
+        File test = new File("babyTextFile.txt");
     
         ArrayList<String> words = new ArrayList<String>(Arrays.asList("a", "b", "c", "d", "e"));
 
-        Map<String, ArrayList<String>> salaries = new HashMap<>();//can't have just Map on left side bc Map is an interface, HashMap implements a map interface
-        salaries.put("Ms. Z", words);//keys must be unique
-        salaries.put("Mr. A", words);
-        salaries.put("Ms. Sandes", words);
-        salaries.put("Person", words);
-        salaries.put("Ross", words);
-        salaries.put("Janice", words);
-        salaries.put("Chandler", words);
-        salaries.put("Chandler", words);//will UPDATE the value at Chandler
+        // Map<String, ArrayList<String>> salaries = new HashMap<>();//can't have just Map on left side bc Map is an interface, HashMap implements a map interface
+        // salaries.put("Ms. Z", words);//keys must be unique
+        // salaries.put("Mr. A", words);
+        // salaries.put("Ms. Sandes", words);
+        // salaries.put("Person", words);
+        // salaries.put("Ross", words);
+        // salaries.put("Janice", words);
+        // salaries.put("Chandler", words);
+        // salaries.put("Chandler", words);//will UPDATE the value at Chandler
 
         //System.out.println(pickBigram(salaries, "Person"));
         //System.out.println(pickBigram(salaries, "Janice"));
 
         System.out.println(scanFile(test));
+
+        writeNewFile(scanFile(test), 4, "test1.txt");
        
 
     }
 
-
+//if the first word has punctiation, make the second word an empty string, and get a value from the empty string key
      public static Map<String, ArrayList<String>> scanFile (File textFile) throws FileNotFoundException {
         Map <String, ArrayList<String>> bigrams = new HashMap<>();//empty map, will be filled with bigrams
+        ArrayList<String> starterWords = new ArrayList<String>();
+        bigrams.put("", starterWords);
         Scanner fileScan = new Scanner(textFile);
         String firstWord = "";
         while(fileScan.hasNext()) { //keep getting strings from file one at a time
-            String secondWord = fileScan.next(); //get the String in the file
-            if (!bigrams.containsKey(firstWord)) { //if firstWord does not exist, add firstWord as a key to the map
+        String secondWord = fileScan.next(); //get the String in the file
+
+            if (hasPunctiation(firstWord)){
+
+                starterWords.add(secondWord);
+
+                bigrams.put("", starterWords);
+
+            }
+
+
+            else if (!bigrams.containsKey(firstWord)) { //if firstWord does not exist, add firstWord as a key to the map
                 ArrayList <String> values = new ArrayList <String> ();
                 values.add(secondWord);
                 bigrams.put(firstWord, values); //add secondWord to the value of firstWord
-                firstWord = secondWord; //change firstWord to SecondWord
             }
+
+
             else {  // if firstWord already exists as a key in the map, get the current value and push the second word to the array list
-                ArrayList temp = bigrams.get(firstWord);
-                temp.add(secondWord);
+                (bigrams.get(firstWord)).add(secondWord);
+                //temp.add(secondWord);
             }
+
+            firstWord = secondWord;//change firstWord to SecondWord
         }
         fileScan.close();
         return bigrams;
     }
+
+
+
      
 
     /*psuedocode
@@ -61,7 +81,7 @@ public class TaraKathrynAIRobot {
 
 
 
-    /*public static void writeNewFile(Map <String, ArrayList<String>> bigrams int numSentences, String fileName) {
+    public static void writeNewFile(Map <String, ArrayList<String>> bigrams, int numSentences, String fileName) throws FileNotFoundException{
 
         ArrayList<String> sentenceEnder = new ArrayList<>(Arrays.asList(".", "?", "!"));
 
@@ -73,19 +93,17 @@ public class TaraKathrynAIRobot {
 
         int curNumSentences = 0;
 
-        while (curNumSentences <= numSentences){
+        while (curNumSentences < numSentences){
 
             newWord = pickBigram(bigrams, curWord);
-            newText += newWord;
+            newText += newWord + " ";
             curWord = newWord;
 
-            for (int i = 0; i < 3; i ++){
-                 if (curWord.indexOf(sentenceEnder.get(i)) != -1){
+                 if (hasPunctiation(curWord)){
                     curWord = "";
                     curNumSentences ++;
                  }
 
-            }
         }
 
         p.print(newText);
@@ -99,12 +117,25 @@ public class TaraKathrynAIRobot {
 1. use .get to get the arraylist(value) of a specific String (key)
 2. Use Math.random to randomely generate a number (withing the indeices of the arrayList). 
 Elements that appear more often in the arraylist will be more likely to show up
-
 */
+
+    public static boolean hasPunctiation(String word){
+
+         ArrayList<String> sentenceEnder = new ArrayList<>(Arrays.asList(".", "?", "!"));
+
+          for (int i = 0; i < 3; i ++){
+                 if (word.indexOf(sentenceEnder.get(i)) != -1){
+                    return true;
+                 }
+
+          }
+
+        return false;
+    }
 
     public static String pickBigram(Map <String, ArrayList <String>> bigrams, String key) {
 
-        return (bigrams.get(key)).get((int) (Math.random() * (bigrams.get(key).size() + 1)));//get the arraylist associated with the key and generate a random number that returns a random index of that arraylist
+        return (bigrams.get(key)).get((int) (Math.random() * (bigrams.get(key).size())));//get the arraylist associated with the key and generate a random number that returns a random index of that arraylist
 
     }
 
