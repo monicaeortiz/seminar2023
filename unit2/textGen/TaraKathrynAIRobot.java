79c52720eaa87ebd04ed033d63e639460a566818
Tara <tsalli@greenwichacademy.org>
2022-12-14 22:50:06 -0500

    FINAL with comments, & cleaned up code

--- unit2/textGen/TaraKathrynAIRobot.java
+++ unit2/textGen/TaraKathrynAIRobot.java
@@ -2,177 +2,82 @@ import java.util.*;
 import java.io.*;
 public class TaraKathrynAIRobot {
 
-<<<<<<< HEAD
-    public static void main (String[] args) throws FileNotFoundException {
-
-        File test = new File("AITest.txt");
-    
-        ArrayList<String> words = new ArrayList<String>(Arrays.asList("a", "b", "c", "d", "e"));
-
-        Map<String, ArrayList<String>> salaries = new HashMap<>();//can't have just Map on left side bc Map is an interface, HashMap implements a map interface
-        salaries.put("Ms. Z", words);//keys must be unique
-        salaries.put("Mr. A", words);
-        salaries.put("Ms. Sandes", words);
-        salaries.put("Person", words);
-        salaries.put("Ross", words);
-        salaries.put("Janice", words);
-        salaries.put("Chandler", words);
-        salaries.put("Chandler", words);//will UPDATE the value at Chandler
-
-        //System.out.println(pickBigram(salaries, "Person"));
-        //System.out.println(pickBigram(salaries, "Janice"));
-
-        System.out.println(scanFile(test));
-       
-=======
+    // MAIN METHOD
     public static void main (String[] args) throws FileNotFoundException, NumberFormatException {
->>>>>>> 8de0fee10fae73b58499cb90bd9103bb6d332d64
-
+        // calls the writeNewFile method, takes in command line arg for file input, num sentences that the user wants to generate, and name of the new file they want to add the new text to
         writeNewFile(scanFile(new File (args[0])), Integer.parseInt(args[1]), args[2]);
     }
 
-//if the first word has punctiation, make the second word an empty string, and get a value from the empty string key
-     public static Map<String, ArrayList<String>> scanFile (File textFile) throws FileNotFoundException {
-        Map <String, ArrayList<String>> bigrams = new HashMap<>();//empty map, will be filled with bigrams
-        ArrayList<String> starterWords = new ArrayList<String>();
-        bigrams.put("", starterWords);
+    //if the first word has punctiation, make the second word an empty string, and get a value from the empty string key
+   // creates a map with all the unique words in the file as keys and their following words as a list of values
+    public static Map<String, ArrayList<String>> scanFile (File textFile) throws FileNotFoundException {
+        Map <String, ArrayList<String>> bigrams = new HashMap<>(); // empty map, will be filled with bigrams
+        ArrayList<String> starterWords = new ArrayList<String>(); // empty arraylist for all the values that start a sentence
+        bigrams.put("", starterWords); // puts the arraylist for starter words as a value for the space
         Scanner fileScan = new Scanner(textFile);
         String firstWord = "";
-        while(fileScan.hasNext()) { //keep getting strings from file one at a time
-<<<<<<< HEAD
-            String secondWord = fileScan.next(); //get the String in the file
-            if (!bigrams.containsKey(firstWord)) { //if firstWord does not exist, add firstWord as a key to the map
-                ArrayList <String> values = new ArrayList <String> (); // makes array list for the values
-                values.add(secondWord); // adds secondWord to the arraylist
-                bigrams.put(firstWord, values); //add the new arraylist to the value of firstWord
-=======
-        String secondWord = fileScan.next(); //get the String in the file
+        while(fileScan.hasNext()) { // keep getting strings from file one at a time
+            String secondWord = fileScan.next(); // get the string in the file
 
-            if (hasPunctiation(firstWord)){
-
-                starterWords.add(secondWord);
-
-                bigrams.put("", starterWords);
-
+            if (hasPunctiation(firstWord)) { // calls the hasPunctuation method to check if the current word could be a word that ends the sentence
+                starterWords.add(secondWord); // if so, the next word would start a new sentence and thus is added to the list of starter words
+                bigrams.put("", starterWords); // the updated list with starter words is added to the map
             }
-
-
-            else if (!bigrams.containsKey(firstWord)) { //if firstWord does not exist, add firstWord as a key to the map
-                ArrayList <String> values = new ArrayList <String> ();
-                values.add(secondWord);
-                bigrams.put(firstWord, values); //add secondWord to the value of firstWord
->>>>>>> 8de0fee10fae73b58499cb90bd9103bb6d332d64
-            }
-
-
-            else {  // if firstWord already exists as a key in the map, get the current value and push the second word to the array list
+            else if (!bigrams.containsKey(firstWord)) { // if firstWord does not exist, add firstWord as a key to the map
+                ArrayList <String> values = new ArrayList <String> (); // creates a list for the values of this unique word
+                values.add(secondWord); // adds the following word to the list
+                bigrams.put(firstWord, values); // add secondWord to the value of firstWord
+            } else {  // if firstWord already exists as a key in the map, get the current value and push the second word to the array list
                 (bigrams.get(firstWord)).add(secondWord);
-                //temp.add(secondWord);
             }
-<<<<<<< HEAD
-            firstWord = secondWord; //change firstWord to SecondWord
-=======
-
             firstWord = secondWord;//change firstWord to SecondWord
->>>>>>> 8de0fee10fae73b58499cb90bd9103bb6d332d64
         }
         fileScan.close();
         return bigrams;
     }
 
-
-
-     
-
-    /*psuedocode
-
-    1. pass in how many words we want the new file to be
-    2. Start with an empty string and get bigrams of the empty string, as those words are likely to be sentence starters. 
-    3. keep on getting the bigram of the word before, until we reach a word with punctuation at the end.
-        increase the number of sentences by q
-        and make the curWord an empty string, so we get a sentence starter word
-
-    */
-
-
-
-    public static void writeNewFile(Map <String, ArrayList<String>> bigrams, int numSentences, String fileName) throws FileNotFoundException{
-
-        ArrayList<String> sentenceEnder = new ArrayList<>(Arrays.asList(".", "?", "!"));
-
+    // writes new text into a new file; generates num sentences that the user desires
+    public static void writeNewFile (Map <String, ArrayList<String>> bigrams, int numSentences, String fileName) throws FileNotFoundException{
+        ArrayList<String> sentenceEnder = new ArrayList<>(Arrays.asList(".", "?", "!")); // creates a list of possible characters that may end a sentence
         PrintStream p = new PrintStream(fileName);
 
-        String newText = "";
+        String newText = ""; 
         String curWord = "";
         String newWord = "";
+        int curNumSentences = 0; 
 
-        int curNumSentences = 0;
-
-        while (curNumSentences < numSentences){
-
-            newWord = pickBigram(bigrams, curWord);
-            newText += newWord + " ";
-            curWord = newWord;
-
-                 if (hasPunctiation(curWord)){
-                    curWord = "";
-                    curNumSentences ++;
-                 }
+        while (curNumSentences < numSentences) { // runs until reached user-inputted desired num of sentences
+            newWord = pickBigram(bigrams, curWord); // calls the pickBigram method to pick a random word from the list that is assigned to the current word and assigns it to newWord
+            newText += newWord + " "; // adds the new word to the text string and a space
+            curWord = newWord; // assigns the new word to current word
+            if (hasPunctiation(curWord)) { // checks if the curren word has punctuation
+                curWord = ""; // if so, assigns current word to a space so that the next word can start a new sentence
+                newText += "\n"; // makes new line for the next sentence in the file so that each sentence is its own line
+                curNumSentences ++; // increments the num of sentences that have been generated
+            }
 
         }
-
         p.print(newText);
-
     }
 
 
-
-/*pseudocode, lerning my lesson slowly but surely
-
-1. use .get to get the arraylist(value) of a specific String (key)
-2. Use Math.random to randomely generate a number (withing the indeices of the arrayList). 
-Elements that appear more often in the arraylist will be more likely to show up
-*/
-
-<<<<<<< HEAD
-    public static String pickBigram(Map <String, ArrayList <String>> bigrams, String key) {
-
-        return (bigrams.get(key)).get((int) (Math.random() * (bigrams.get(key).size() + 1)));//get the arraylist associated with the key and generate a random number that returns a random index of that arraylist
-        // if it has punctuation, get word from the empty string key
-    }
-
-
-    // public static String checkPunctuation(Map <String, ArrayList <String>> bigrams) {
-
-
-
-    // }
-
-
-/* do while loop
-
-get the first word in the file
-add it to the map
-=======
-    public static boolean hasPunctiation(String word){
->>>>>>> 8de0fee10fae73b58499cb90bd9103bb6d332d64
-
-         ArrayList<String> sentenceEnder = new ArrayList<>(Arrays.asList(".", "?", "!"));
-
-          for (int i = 0; i < 3; i ++){
-                 if (word.indexOf(sentenceEnder.get(i)) != -1){
+    // checks if the current word has punctuation attached to it (does it end a sentence) and returns a boolean
+    public static boolean hasPunctiation (String word) {
+        ArrayList<String> sentenceEnder = new ArrayList<>(Arrays.asList(".", "?", "!")); // creates a list of possible characters that may end a sentence
+            for (int i = 0; i < 3; i ++) { // iterates through the list
+                if (word.indexOf(sentenceEnder.get(i)) != -1){ // checks if the word has any of the 3 possible sentence ender punctuation
                     return true;
-                 }
+                }
 
-          }
-
+            }
         return false;
     }
 
-    public static String pickBigram(Map <String, ArrayList <String>> bigrams, String key) {
 
-        return (bigrams.get(key)).get((int) (Math.random() * (bigrams.get(key).size())));//get the arraylist associated with the key and generate a random number that returns a random index of that arraylist
 
+    // for each word, picks a random value from the list assigned to the key word and returns it
+    public static String pickBigram (Map <String, ArrayList <String>> bigrams, String key) {
+        return (bigrams.get(key)).get((int) (Math.random() * (bigrams.get(key).size())));// get the list associated with the key and generate a random number that returns a random index of that arraylist
     }
 
 
