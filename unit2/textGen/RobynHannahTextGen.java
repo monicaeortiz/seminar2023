d43e9ea10ce67100bb5ad11dd7c6e472ede3b8c8
hdwyerr <hedwyer20@gmail.com>
2023-01-05 07:49:15 -0500

    final code with comments

--- unit2/textGen/RobynHannahTextGen.java
+++ unit2/textGen/RobynHannahTextGen.java
@@ -7,22 +7,21 @@ public class RobynHannahTextGen {
         System.out.println(populateMap(f));
         Map<String, List<String>> bigrams = populateMap(f); 
         
-        sentenceBuilder(bigrams, "smallTest.txt", 10); 
+        sentenceBuilder(bigrams, 10); //generating 10 sentences of a sample 
 
     
     }
 
     //method to print output text to prinstream
-    public static void sentenceBuilder (Map<String, List<String>> bigrams, String fileName, int numSentences) throws FileNotFoundException {
+    public static void sentenceBuilder (Map<String, List<String>> bigrams, int numSentences) throws FileNotFoundException {
         String sentence = "";
         int sentenceCounter =0;
-        String generatedFile = "hdGeneratedFile.txt";
+        String generatedFile = "hdGeneratedFile.txt";//new file to write generated text to
         PrintStream p = new PrintStream(new FileOutputStream(generatedFile, true));
         while(sentenceCounter < numSentences){
             //gets a starter word by getting random word from list of words that follow an empty space 
             String firstWord = getRandomElement(bigrams.get("")); 
             sentence += firstWord + " "; 
-            //above does the first word of every sentence
 
             Set<String> keySet = bigrams.keySet(); 
             List<String> keys = new ArrayList<String>(keySet); //list of all keys 
@@ -31,55 +30,38 @@ public class RobynHannahTextGen {
                 //get random word from the list of followers of firstWord
                 nextWord = getRandomElement(bigrams.get(firstWord));
                 sentence += nextWord + " ";
-                firstWord = nextWord; 
-                //above steps need to repeat until nextWord has punctuation
+                firstWord = nextWord; //make firstWord nextWord so above steps can keep repeating for each new word until it ends a sentence
             }
-            /*while (!endsSentence(firstWord)) {
-            String nextWord = getRandomElement(bigrams.get(firstWord)); 
-            sentence += nextWord + " "; 
-            nextWord = getRandomElement(bigrams.get(nextWord));
-            }*/
-            sentenceCounter ++;
-            
-            
+            sentenceCounter ++;//keeping track of how many sentences have been generated 
         }
         p.print(sentence);
-        //get a random value from the list of "" to be the first word of the sentence. add to string
-        //nextWord = bigrams.get(firstWord) -- gets value list of previos word
-        //now get a random value from nextWord's value list and add to string. 
-        p.close();
+        p.close(); 
     }
     
     //method to fill bigram map from input file
     public static Map<String, List<String>> populateMap (File f) throws FileNotFoundException{
-        //will take input text and create bigrams map 
+        
         Map<String, List<String>> bigrams = new HashMap<>(); 
-        //make a new map
-        //make scanners to read file and lines
-        //make var called first word andmake empty string 
-        //in the loop, put key and then values
         String word = "";
         Scanner scanner = new Scanner(f); 
         List<String> valueList = new ArrayList<>();
         while(scanner.hasNext()){
-            //don't call next twice 
+            //getting each token in the file 
             String currentKey = scanner.next();
-            // check if current Key is in dictionary 
+            // check if currentKey is in dictionary 
             if(bigrams.containsKey(word) == false){//if key is not already in the list
-                bigrams.put(word,  new ArrayList<String>(Arrays.asList(currentKey)));
+                bigrams.put(word,  new ArrayList<String>(Arrays.asList(currentKey)));// you need to make a new arrayList since this is the first time the key is appearing
             }
 
-            else{//if key is in the list 
-            //change the list first, then add the new list as the value
+            else{//if key is already in the list 
                 //change the list first, then add the new list as the value
                 valueList = bigrams.get(word);
                 valueList.add(currentKey);
                 bigrams.put(word,  valueList);
-
             }
 
             if (endsSentence(currentKey) ) {
-                word = ""; 
+                word = ""; //this makes sure that all first words of a sentence will follow the empty space key in the map
             }
             else {
                 word = currentKey;
@@ -94,9 +76,7 @@ public class RobynHannahTextGen {
         //checks if a word has sentence ending punctuation, and returns true or false
         if(word.indexOf(".") == -1 && word.indexOf("!") == -1 && word.indexOf("?") == -1) {//if the word does not have punctuation
             return false;
-        }
-        //System.out.println(bigrams);
-        //return bigrams; 
+        } 
         return true;
     }
 
